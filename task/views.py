f6e2ce67d4aded5b80d3fed2b6350cfd8ff55d82
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-02-03 21:24:12 +0200

    add num of elements to index view funktion & index.html

--- task/views.py
+++ task/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
+from .models import Project, Team, Worker
+
 
 def index(request):
     """View function for home page."""
-    return render(request, "task/index.html")
+    num_projects = Project.objects.count()
+    num_teams = Team.objects.count()
+    num_workers = Worker.objects.count()
+
+    context = {
+        "num_projects": num_projects,
+        "num_teams": num_teams,
+        "num_workers": num_workers,
+    }
+
+    return render(request, "task/index.html", context=context)
